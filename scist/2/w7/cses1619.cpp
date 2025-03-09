1ed1121cb8429151689d10dfaf725d5073aadc4e
Hu Kaixiang <maxhu787@gmail.com>
2025-03-09 16:55:26 +0800

    ee

---
+++ scist/2/w7/cses1619.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+  int n;
+  cin >> n;
+
+  vector<int> v(0);
+  while(n--) {
+    int a, b;
+    cin >> a >> b;
+  }
+  return 0;
+}
