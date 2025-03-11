19cdd0c6451d98768fb7d8c21f903951cbda90a2
Hu Kaixiang <maxhu787@gmail.com>
2025-03-11 12:14:25 +0800

    e

--- scist/2/w6/單調性/cses1641.cpp
+++ scist/2/w6/單調性/cses1641.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
+#include <utility>
+#include <map>
 using namespace std;
 
 int main() {
+  int n, x;
+  cin >> n >> x;
+
+  vector<pair<int, int>> a;
+  for(int i = 0; i < n; i++) {
+    int t;
+    cin >> t;
+    a.push_back(pair<int, int> {t, i})
+  }
   return 0;
 }
