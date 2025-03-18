998e6bb5300b824b8e6e37429b0d945324a0439d
Hu Kaixiang <maxhu787@gmail.com>
2025-03-18 15:58:06 +0800

    e

--- scist/1/w5/synchronizinglists.cpp
+++ scist/1/w5/synchronizinglists.cpp
@@ -1,6 +1,32 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
+#include <map>
 using namespace std;
 
 int main() {
+  int n;
+  bool first = true;
+  while(cin >> n && n != 0) {
+    if(!first) cout << "\n";
+    first = false;
+    map<int, int> m;
+    
+    vector<int> a(n), b(n), o(n);
+    for(int i = 0; i < n; i++) {
+      int t; cin >> t;
+      a[i] = t; o[i] = t;
+    }
+    for(int &i : b) cin >> i;
+    
+    sort(a.begin(), a.end());
+    sort(b.begin(), b.end());
+
+    for(int i = 0; i < n; i++) {
+      m[a[i]] = b[i];
+    }
+
+    for(auto i : o) cout << m[i] << "\n";
+  }
   return 0;
 }
