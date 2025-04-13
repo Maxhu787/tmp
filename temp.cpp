2ad8369d1c3464a812d50023a135e2685dd86a69
Hu Kaixiang <maxhu787@gmail.com>
2025-04-13 13:00:35 +0800

    h

--- temp.cpp
+++ temp.cpp
@@ -1,7 +1,26 @@
 #include <iostream>
 using namespace std;
+#define ILikeLoli ios_base::sync_with_stdio(0);
+#define LoliSoCute cin.tie(0); cout.tie(0);
 
-int main() {
+#define ll long long
+#define pii pair<int, int>
+#define f first
+#define s second
+const int mxn = 2e5 + 3;
 
-  return 0;
+void solve() {
+  int n;
+  cin >> n;
+}
+
+signed main(){
+    ILikeLoli LoliSoCute
+    int t;
+    cin >> t;
+    while(t--) {
+      solve();
+    }   
+    
+    return 0;
 }
