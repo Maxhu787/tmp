6f3f026c0899e3ab197a86c415ba7b3f00ec55e3
Hu Kaixiang <maxhu787@gmail.com>
2025-04-15 07:36:58 +0800

    e

--- scist/1/w4/--summertrip.cpp
+++ scist/1/w4/--summertrip.cpp
@@ -1,22 +1,26 @@
 #include <iostream>
+#include <set>
 #include <string>
-#include <vector>
 using namespace std;
 
+typedef long long ll;
+
+set<int> st[30];
+
 int main() {
-  string s;
-  cin >> s;
-  
-  int a[26] = {0};
-  for(char i : s) a[i-'a']++;
+    string s;
+    cin >> s;
+    ll ans = 0;
 
+    for (int i = 0; i < s.size(); i++) {
+        int x = s[i] - 'a' + 1;
+        ans += st[x].size();
+        for (int j = 1; j <= 26; j++) {
+            st[j].insert(x);
+        }
+        st[x].clear();
+    }
 
-  return 0;
+    cout << ans << endl;
+    return 0;
 }
-
-/*
-
-abbcccddddeeeeea
-thenumberofgoodstringsis
-
-*/
