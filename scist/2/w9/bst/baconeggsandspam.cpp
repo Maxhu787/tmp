f5c3e5e47c001e70d2903340e198eb81d1e5e10f
Hu Kaixiang <maxhu787@gmail.com>
2025-05-01 20:05:40 +0800

    e

--- scist/2/w9/bst/baconeggsandspam.cpp
+++ scist/2/w9/bst/baconeggsandspam.cpp
@@ -1,7 +1,37 @@
 #include <iostream>
+#include <string>
+#include <sstream>
+#include <map>
+#include <set>
 using namespace std;
 
 int main() {
-  
+  int n;
+  while (cin >> n && n != 0) {
+    map<string, set<string>> mp;
+    cin.ignore();
+
+    for (int i = 0; i < n; i++) {
+      string s;
+      getline(cin, s);
+      stringstream ss(s);
+      string name, item;
+      ss >> name;
+      while (ss >> item) {
+        mp[item].insert(name);
+      }
+    }
+
+    for(auto it = mp.begin(); it != mp.end(); it++) {
+      cout << it->first;
+      for (auto name : it->second) {
+        cout << " " << name;
+      }
+      cout << "\n";
+    }
+
+    cout << "\n";
+  }
+
   return 0;
 }
