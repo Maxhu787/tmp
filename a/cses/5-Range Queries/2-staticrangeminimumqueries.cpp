77090b1158969775d49587230f4737dd45a309d4
Hu Kaixiang <maxhu787@gmail.com>
2025-08-05 22:44:02 +0800

    ac!!!

--- a/cses/5-Range Queries/2-staticrangeminimumqueries.cpp
+++ a/cses/5-Range Queries/2-staticrangeminimumqueries.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <climits>
+#include <algorithm>
 using namespace std;
 
 typedef long long ll;
@@ -13,16 +15,16 @@ void build(int l, int r, int id = 1) {
     return ;
   }
   int m = (l + r) >> 1;
-  build(l, m, 2*id);
-  build(m+1, r, 2*id + 1);
-  seg[id] = seg[id*2] + seg[id*2+1];
+  build(l, m, id * 2);
+  build(m+1, r, id*2+1);
+  seg[id] = min(seg[id*2], seg[id*2+1]);
 }
 
 ll query(int ql, int qr, int l, int r, int id = 1) {
-  if(qr < l || ql > r) return 0;
+  if(qr < l || ql > r) return LLONG_MAX;
   if(ql <= l && qr >= r) return seg[id];
   int m = (l + r) >> 1;
-  return query(ql, qr, l, m, id*2) + query(ql, qr, m+1, r, id*2+1);
+  return min(query(ql, qr, l, m, id*2), query(ql, qr, m+1, r, id*2+1));
 }
 
 int main() {
--- templates/segtree-1.cpp
+++ templates/segtree-1.cpp
@@ -21,13 +21,15 @@ void build(int l, int r, int id = 1) {
 }
 
 int query(int ql, int qr, int l, int r, int id = 1) {
-  if(qr < l || r < ql) return 0;
-  if(ql <= l && r <= qr) return seg[id];
+  if(qr < l || r < ql) return 0; // no overlap
+  if(ql <= l && r <= qr) return seg[id]; // full overlap
   int m = (l+r) / 2;
   return query(ql, qr, l, m, id*2) + query(ql, qr, m+1, r, id*2+1);
+  // partial overlap
 }
 
 void update(int p, int v, int l, int r, int id = 1) {
+  // p: position, v: value
   if(p < l || r < p) return;
   if(l == r) {
     seg[id] = v;
