592f4470532fc2d0631b31aaa0f4bd9e4341048a
Hu Kaixiang <maxhu787@gmail.com>
2025-11-03 22:29:41 +0800

    e

---
+++ apcs-course/8/a077--.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+const int mxn = 2e5;
+int a[4*mxn];
+
+
+int main() {
+  int t;
+  cin >> t;
+
+  while(t--) {
+    memset(a, 0, sizeof(a));
+    int d, l;
+    cin >> d >> l;
+    for(int i = 0; i < l; i++) {
+      for(int j = 0; j < d; j++) {
+        ;
+      }
+    }
+  }
+  return 0;
+}
