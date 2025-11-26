c497882547f2263af80f793e8dcd15e407e0ab40
Hu Kaixiang <maxhu787@gmail.com>
2025-11-26 23:38:35 +0800

    e

--- cses/2-Sorting and Searching/4-concerttickets.cpp
+++ cses/2-Sorting and Searching/4-concerttickets.cpp
@@ -7,21 +7,39 @@ int main() {
   int n, m;
   cin >> n >> m;
 
-  vector<int> a(n), c(m);
-  for(int &i : a) cin >> i;
+  vector<int> ticket(n), c(m);
+  for(int &i : ticket) cin >> i;
   for(int &i : c) cin >> i;
 
-  sort(a.begin(), a.end());
-  int pa=0, pc=0;
-  while(pa < n && pc < m) {
-    ;
+  sort(ticket.begin(), ticket.end());
+  int pt=0, pc=0, current=-1;
+  while(pt <= n && pc <= m) {
+    if(pt == n || pc == m) {
+      cout << current << "\n";
+      for(int i = pc+1; i < m; i++) {
+        cout << -1 << "\n";
+      }
+      break;
+    }
+    if(ticket[pt] > c[pc]) {
+      if(current == -1) {
+        cout << -1 << " " << current << "\n";
+      }
+      cout << current << " " << current << "\n";
+      pc++;
+    }
+    current = ticket[pt];
+    pt++;
   }
   return 0;
 }
 
 /*
+ticket 3 5 5 7 8
+     c 4 8 3 
+
+
+5 3
+5 5 7 8 5
 4 8 3
-3 5 5 7 8
-
-
 */
