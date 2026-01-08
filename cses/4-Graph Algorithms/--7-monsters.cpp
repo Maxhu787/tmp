02d297bb9b617e135e8e0a4c5fd8391602087ca6
Hu Kaixiang <maxhu787@gmail.com>
2026-01-08 13:05:18 +0800

    w

--- cses/4-Graph Algorithms/--7-monsters.cpp
+++ cses/4-Graph Algorithms/--7-monsters.cpp
@@ -8,13 +8,12 @@ pii mp[4] = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
 const int mxn = 1e3+1;
 const int mxm = 1e3+1;
 pii parent[mxn][mxm];
+int grid[mxn][mxm];
+int n, m, oi, oj;
+deque<pii> q;
 
 int main() {
-  int n, m;
   cin >> n >> m;
-
-  deque<pii> q;
-  int grid[n][m];
   for(int i = 0; i < n; i++) {
     for(int j = 0; j < m; j++) {
       char c;
@@ -35,12 +34,11 @@ int main() {
   }
 
   // path: 0; wall: 1; monster: 2; player: 3;
-  int oi, oj;
   while(!q.empty()) {
     auto u = q.front();
     q.pop_front();
 
-    cout << u.first+1 << " " << u.second+1 << "\n";
+    cout << u.first+1 << " " << u.second+1 << ": "; //
     bool monster = false;
     if(grid[u.first][u.second] == 2) monster = true;
     for(auto i : mp) {
@@ -48,30 +46,38 @@ int main() {
       newi = u.first + i.first;
       newj = u.second + i.second;
       if(newi < n && newi >= 0 && newj < m && newj >= 0) {
+        cout << "(" << newi+1 << " " << newj+1 << ") "; //
         if(monster) {
           if((grid[newi][newj] == 0 || grid[newi][newj] == 3)) {
             // path, player
+            cout << "yes "; //
             grid[newi][newj] = 2;
             q.push_back({newi, newj});
+          } else {
+            cout << "no "; //
           }
         } else {
           if(grid[newi][newj] == 0) {
             // path
+            cout << "yes "; //
             grid[newi][newj] = 3;
             parent[newi][newj] = {u.first, u.second};
             q.push_back({newi, newj});
 
             if(newi == n-1 || newi == 0 || newj == m-1 || newj == 0) {
               oi = newi; oj = newj;
-              cout << "found" << "\n";
+              cout << "---------found"; //
               break;
             }
+          } else {
+            cout << "no "; //
           }
         }
       }
     }
+    cout << "\n"; //
   }
-  cout << "---\n";
+  cout << "---\n"; //
   for(int a = oi, b = oj; a != -1 && b != -1; a = parent[a][b].first, b = parent[a][b].second) {
     cout << a+1 << " " << b+1 << "\n";
   }
